22a7bed68f222bf2f43e97ab913c2a89a7aac8f8
Jonathan Vega <33010830+MrZ3T4@users.noreply.github.com>
2026-01-01 12:15:56 -0600

    mediatek: wlan: simplify WAPI cert handling
    Remove cert_list alias checks to fix compilation errors in wpa_supplicant.

--- wlan/wpa_supplicant_8_lib/wapi.c
+++ wlan/wpa_supplicant_8_lib/wapi.c
@@ -241,10 +241,7 @@ int wapi_set_suites(struct wpa_supplicant* wpa_s, struct wpa_ssid* ssid, struct 
         }
         wapi_param.kv[wapi_param.kl] = '\0';
     } else if (wpa_s->key_mgmt == WPA_KEY_MGMT_WAPI_CERT) {
-        if (!ssid->wapi_cert_alias || !os_strcmp((char*)ssid->wapi_cert_alias, "NULL"))
-            wapi_param.cert_list = NULL;
-        else
-            wapi_param.cert_list = (char*)ssid->wapi_cert_alias;
+        wapi_param.cert_list = NULL;
         wapi_param.authType = AUTH_TYPE_WAPI_CERT;
         wpa_printf(MSG_INFO, "[WAPI] Using Key mgmt WAPI Cert");
     }
